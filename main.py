fb85586615e916024e1829815587607ed0b8dd35
neeravgade <38474039+neeravgade@users.noreply.github.com>
2018-05-13 21:25:20 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-'''I will create a game with pygame. Player will be able to go through multiple rooms and walls
-and try to avoid obstacles. I'm not sure what obstacles I want to add, but they will get progressively tougher
-as the player navigates past levels. I will start with the mazerunner.py code I found online and change it
-significantly to make the game harder. 
-'''
 #i followed the tutorial on a website for this 
 #the url is http://programarcadegames.com/python_examples/show_file.php?file=maze_runner.py
 import pygame
-import pygame, sys
+import sys
+import random
+import math
+import os
 from pygame.locals import *
-
+import signal
+import time
 
 #these are RGB values (in that order)
 BLACK = (0, 0, 0)
@@ -17,10 +16,21 @@ BLUE = (0, 0, 204)
 GREEN = (0, 204, 0)
 RED = (255, 0, 0)
 PURPLE = (204, 0, 204)
-YELLOW = (255, 255, 0)
+GOLD = (255, 215, 0)
 
+# init pygame
+pygame.init()
+# init sound mixer
+pygame.mixer.pre_init(44100, 16, 2, 4096)
+pygame.mixer.init()
 
+# background music
+pygame.mixer.music.load("JS.mp3.mp3")
+pygame.mixer.music.set_volume(1)
 
+# loop background music and make it play
+pygame.mixer.music.play(loops=-1)
+pygame.mixer.music.set_volume(.5)
 
 #this gets the background image from the directory
 imr_dir = "C:/Users/Neerav.Gade18/OneDrive - Bellarmine College Preparatory/CompPrgm/finalPROJECT/galaxy-run/galaxy-run"
@@ -39,11 +49,28 @@ class Wall(pygame.sprite.Sprite):
         self.image = pygame.Surface([width, height])
         self.image.fill(color)
  
-        # Make our top-left corner the passed-in location.
+        # Make our top-left corner the passed-in location
         self.rect = self.image.get_rect()
         self.rect.y = y
         self.rect.x = x
  
+class Coin(pygame.sprite.Sprite):
+    #this class represents the group of "coins" on the screen
+    #i can use the same format as the wall class
+    def __init__(self, x, y):
+        """ Constructor function """
+ 
+        # Call the parent's constructor
+        super().__init__()
+ 
+        # Make a GOLD square that the Player sprite can collect
+        self.image = pygame.Surface([15, 15])
+        self.image.fill(GOLD)
+ 
+        # Make our top-left corner the passed-in location
+        self.rect = self.image.get_rect()
+        self.rect.y = y
+        self.rect.x = x
  
 class Player(pygame.sprite.Sprite):
     #This class represents the sprite that the player controls
@@ -66,16 +93,21 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.y = y
         self.rect.x = x
+        self.score = 0
  
     def changespeed(self, x, y):
-        """ Change the speed of the player. Called with a keypress. """
+        #Change the speed of the player. Called with a keypress. 
         self.change_x += x
         self.change_y += y
  
+    def move(self, walls, coins):
+        #coin_collect_list is the group of gold coins on the screen. The spritecollide function within pygame
+        #automatically checks to see if the sprite collides with the gold coins. If so, kill the coin and add 1 to score.
+        coin_collect_list = pygame.sprite.spritecollide(self, coins, False)
+        for coin in coin_collect_list:
+            coin.kill()
+            self.score += 1
 
-    def move(self, walls):
-        """ Find a new position for the player """
- 
         # Move left/right
         self.rect.x += self.change_x
  
@@ -85,13 +117,11 @@ class Player(pygame.sprite.Sprite):
             # If we are moving right, set our right side to the left side of
             # the item we hit
             if self.change_x > 0:
-                self.image.fill(RED)
                 self.rect.right = block.rect.left
             else:
                 # Otherwise if we are moving left, do the opposite.
                 self.rect.left = block.rect.right
             
- 
         # Move up/down
         self.rect.y += self.change_y
  
@@ -105,22 +135,20 @@ class Player(pygame.sprite.Sprite):
             else:
                 self.rect.top = block.rect.bottom
  
- 
 class Room(object):
     """ Base class for all rooms. """
  
-    # Each room has a list of walls, and of enemy sprites.
+    # Each room has a list of walls, and of coin sprites.
     wall_list = None
-    enemy_sprites = None
- 
+    coin_list = None
+
     def __init__(self):
-        """ Constructor, create our lists. """
+        #Constructor, create our lists.
         self.wall_list = pygame.sprite.Group()
-        self.enemy_sprites = pygame.sprite.Group()
- 
+        self.coin_list = pygame.sprite.Group()
  
 class Room1(Room):
-    """This creates all the walls in room 1"""
+    #This creates all the walls in room 1
     def __init__(self):
         super().__init__()
         # Make the walls. (x_pos, y_pos, width, height)
@@ -132,14 +160,25 @@ class Room1(Room):
                  [780, 350, 20, 250, BLUE], #right bottom
                  [20, 0, 760, 20, BLUE], #entire top
                  [20, 580, 760, 20, BLUE], #entire bottom
-                 [390, 50, 20, 500, BLUE] #this is the wall in the middle
+                 [390, 50, 20, 500, WHITE] #this is the wall in the middle
                 ]
- 
+
+        #the same way we made walls, this is the list for coins BUT we are using random function to randomize 
+        #where the coins are on the screen each time program runs.
+        coins = []
+        for i in range(20):
+            x = random.randint(40,760)
+            y = random.randint(40,560)
+            coins.append([x,y])
+
+        for item in coins:
+            coin = Coin(item[0], item[1])
+            self.coin_list.add(coin)
+
         # Loop through the list. Create the wall, add it to the list
         for item in walls:
             wall = Wall(item[0], item[1], item[2], item[3], item[4])
             self.wall_list.add(wall)
- 
  
 class Room2(Room):
     """This creates all the walls in room 2"""
@@ -152,26 +191,35 @@ class Room2(Room):
                  [780, 350, 20, 250, PURPLE],
                  [20, 0, 760, 20, PURPLE],
                  [20, 580, 760, 20, PURPLE],
-                 [190, 50, 20, 500, GREEN],
-                 [590, 50, 20, 500, GREEN]
+                 [190, 50, 20, 500, WHITE],
+                 [590, 50, 20, 500, WHITE]
                 ]
  
         for item in walls:
             wall = Wall(item[0], item[1], item[2], item[3], item[4])
             self.wall_list.add(wall)
- 
- 
+
+        coins = []
+        for i in range(20):
+            x = random.randint(40,760)
+            y = random.randint(40,560)
+            coins.append([x,y])
+
+        for item in coins:
+            coin = Coin(item[0], item[1])
+            self.coin_list.add(coin)
+
 class Room3(Room):
     """This creates all the walls in room 3"""
     def __init__(self):
         super().__init__()
  
-        walls = [[0, 0, 20, 250, WHITE], 
-                 [0, 350, 20, 250, WHITE], 
-                 [780, 0, 20, 250, WHITE],
-                 [780, 350, 20, 250, WHITE],
-                 [20, 0, 760, 20, WHITE],
-                 [20, 580, 760, 20, WHITE]
+        walls = [[0, 0, 20, 250, RED], 
+                 [0, 350, 20, 250, RED], 
+                 [780, 0, 20, 250, RED],
+                 [780, 350, 20, 250, RED],
+                 [20, 0, 760, 20, RED],
+                 [20, 580, 760, 20, RED]
                 ]
  
         for item in walls:
@@ -180,15 +228,24 @@ class Room3(Room):
  
         for x in range(100, 800, 100):
             for y in range(50, 451, 300):
-                wall = Wall(x, y, 20, 200, RED)
+                wall = Wall(x, y, 20, 200, WHITE)
                 self.wall_list.add(wall)
  
         for x in range(150, 700, 100):
-            wall = Wall(x, 200, 20, 200, BLUE)
+            wall = Wall(x, 200, 20, 200, WHITE)
             self.wall_list.add(wall)
  
+        coins = []
+        for i in range(20):
+            x = random.randint(40,760)
+            y = random.randint(40,560)
+            coins.append([x,y])
 
+        for item in coins:
+            coin = Coin(item[0], item[1])
+            self.coin_list.add(coin)
 
+#MAIN FUNCTION
 def main():
     """ Main Program """
  
@@ -221,17 +278,23 @@ def main():
     current_room = rooms[current_room_no]
  
     clock = pygame.time.Clock()
- 
-    done = False
+    #load background image
     background_image = pygame.image.load("stars.png").convert()
-    while not done:
- 
-        # --- Event Processing ---
- 
+
+    #represents the first position of the moving background image
+    x=0
+
+    #make the game run for 50 seconds then quit program
+    start = time.time()
+    while time.time() - start < 50:
+        #put the score in the top left corner of the game
+        pygame.display.set_caption("Score: " + str(player.score))
+
+        #event processing
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                done = True
- 
+                pygame.quit()
+            #the x,y speed for when each key is pressed down
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     player.changespeed(-5, 0)
@@ -241,7 +304,9 @@ def main():
                     player.changespeed(0, -5)
                 if event.key == pygame.K_DOWN:
                     player.changespeed(0, 5)
- 
+                if event.key == pygame.K_q:
+                    pygame.quit()
+            #the x,y speed for when each key is NOT pressed down
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT:
                     player.changespeed(5, 0)
@@ -252,9 +317,8 @@ def main():
                 if event.key == pygame.K_DOWN:
                     player.changespeed(0, -5)
  
-        # --- Game Logic ---
- 
-        player.move(current_room.wall_list)
+        # Game Logic
+        player.move(current_room.wall_list,current_room.coin_list)
  
         if player.rect.x < -15:
             if current_room_no == 0:
@@ -284,18 +348,21 @@ def main():
                 current_room = rooms[current_room_no]
                 player.rect.x = 0
  
-        # ---background and sprites ---
-        
-        screen.blit(background_image, [0,0])
-     
+        #background and sprites
+        screen.blit(background_image, [x,0])
+        x-=1
 
+        #draw the sprites on the screen
         movingsprites.draw(screen)
         current_room.wall_list.draw(screen)
- 
+        current_room.coin_list.draw(screen)
+
         pygame.display.flip()
- 
-        clock.tick(100)
- 
+
+        #60 frames per second
+        clock.tick(60)
+
+
     pygame.quit()
  
 if __name__ == "__main__":
